b2e278f28c84a9d41efd5e9fb72c5ea8a9330dcb
Panagiotis Kouzaris <panagiotis.kouzaris@gmail.com>
2022-07-18 22:17:17 +0300

    LAZY ALERT, ONLY FOR LAZY PEOPLE

---
+++ lazyrequirementsinstall.py
@@ -0,0 +1,14 @@
+import os
+
+if os.name == "nt":
+    os.system('cmd /c "pip install -r requirements.txt > out.txt"')
+    with open("out.txt") as W:
+        text = W.read()
+        print(text)
+else:
+    os.system('cmd /c "pip3 install -r requirements.txt > out.txt"')
+    with open("out.txt") as W:
+        text = W.read()
+        print(text)
+
+input("Press enter to exit...")
